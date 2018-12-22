dbc6cac11f86dc67485bd16ea18943ee7a070f6f
caturnugroho <45826024+caturnugroho@users.noreply.github.com>
2018-12-23 01:53:08 +0700

    complete code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FlashCardComponent } from './../components/flash-card/flash-card';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -10,7 +11,8 @@ import { HomePage } from '../pages/home/home';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FlashCardComponent
   ],
   imports: [
     BrowserModule,
